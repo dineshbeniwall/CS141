7d66cb36407656497ead390183ab46fe8e4a9383
dineshbeniwall <42206773+dineshbeniwall@users.noreply.github.com>
2018-08-17 21:20:01 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,27 @@
+//including library
+#include<iostream>
+
+using namespace std;
+
+int main(){
+//let's begain
+cout <<"Alzebra by c++"<<endl;
+//veriables
+int x=10;
+int y=2;
+//defining veriables with opretors 
+int a= x+y;
+int b= x-y;
+int c= x*y;
+int e= x/y;
+int f= (y%x);
+
+cout<<"sum is "<<a<<endl;
+cout<<"diffrence is "<<b<<endl;
+cout<<"product is "<<c<<endl;
+cout<<"dividation is "<<e<<endl;
+cout<<"modulus is "<<f<<endl;
+return 0;
+//all done
+
+}
