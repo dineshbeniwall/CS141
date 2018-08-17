7ad221097c2b261d2fb084869f033c26c7742805
dineshbeniwall <42206773+dineshbeniwall@users.noreply.github.com>
2018-08-17 21:11:19 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,28 @@
+// include library
+#include<iostream>
+
+using namespace std;
+// using main function
+int main(){
+// declering values
+int a=5;
+bool b=1;
+char x=22;
+short c=110;
+long d=558;
+long long e=1006;
+float f=22.55;
+double g=329;
+long double h=222;
+// find size
+cout<<"size of integer "<<sizeof(a)<<endl;
+cout<<"size of bool "<<sizeof(b)<<endl;
+cout<<"size of char "<<sizeof(x)<<endl;
+cout<<"size of short "<<sizeof(c)<<endl;
+cout<<"size of long "<<sizeof(d)<<endl;
+cout<<"size of long long "<<sizeof(e)<<endl;
+cout<<"size of float "<<sizeof(f)<<endl;
+cout<<"size of double "<<sizeof(g)<<endl;
+cout<<"size of long double "<<sizeof(h)<<endl;
+return 0;
+}
