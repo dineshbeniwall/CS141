fdbd36a1f47d2281e9fb2e313d16ce20e97e9f3d
dineshbeniwall <42206773+dineshbeniwall@users.noreply.github.com>
2018-08-22 05:06:33 +0000

    Add files via upload

---
+++ lab4_q_5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+
+int main(){
+
+//right angle triangle or pythagours theorm
+
+	float a,b,c;		//a,b,c is angles of a triangle
+				// it's simple after question 1 complete
+	cout << "enter angles : \n";
+	
+	cin >> a;
+	cin >> b;
+
+	c = 180-(a+b);
+	cout << "your third angle of a triangle is : "<<c<<endl;
+return 0;
+
+
+
+
+
+
+}
