5ca681ddf47dc8ec47e4cc833de8f75540bb0720
dineshbeniwall <42206773+dineshbeniwall@users.noreply.github.com>
2018-08-22 05:35:26 +0000

    Add files via upload

---
+++ lab4_q_2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main(){
+
+
+	float celsius,fahrenheit;	//only two veriables
+					//ask me how hot today
+	cout << "How are you" <<endl<<"what is today's temperature"<<endl;
+					//now i tell her temp.
+	cin >> celsius;			//it's formula time
+	fahrenheit = (celsius*9)/5+32;	
+
+	cout <<"Have a nice day, today's temp. in fahrenheit is : "<<fahrenheit<<endl;
+					//today too hot isn't it.
+
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+}
