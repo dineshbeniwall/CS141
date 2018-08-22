90a0a31e83982a790fccc435474ae98561b4b348
dineshbeniwall <42206773+dineshbeniwall@users.noreply.github.com>
2018-08-22 05:20:02 +0000

    Add files via upload

---
+++ lab4_q_6.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+
+	float height,base,area; //wright all veriables
+
+	cout << "write hegiht of triangle \n"; //ask one by one first hight and then base
+	
+	cin >> height;
+	cout << "write base of your triange \n";
+	cin >> base;				//all complete now formula
+
+	area = (height*base)/2;
+	cout << "Area of your triangle is : "<<area<<endl;
+return 0;					//let's compile
+
+
+
+
+}
